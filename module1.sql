8e19700e43ed1755d1c8a224d44dce1542a95826
QuentinGeerts <quentin.geerts@bstorm.be>
2022-02-10 11:20:45 +0100

    Correction Module 1

--- module1.sql
+++ module1.sql
@@ -9,8 +9,9 @@
 -- Créer une table ayant une colonne de type INTEGER, auto-incrémentée.
 -- Où cette table s’est-elle créée ? Comment pouvez-vous la visualiser ?
 
-CREATE TABLE nom_table (
+CREATE TABLE nom_table2 (
   colonne INT AUTO_INCREMENT,
+  text VARCHAR(50),
   KEY colonne_key (colonne)
 );
 
@@ -30,17 +31,24 @@ CREATE TABLE nom_table (
 -- 3. A l’aide de l’instruction « SELECT », afficher le message « Bonjour, 
 -- et bienvenue dans le cours de Transact SQL ! »
 
-
-SELECT "Bonjour, et bienvenue dans le cours de SQL Procédural"
+SELECT "Bonjour, et bienvenue dans le cours de SQL Procédural";
 
 -- 4. Insérer une ligne de valeurs dans la table créée au point 1.2.
 -- Insérer une seconde ligne identique. Un message d’erreur doit apparaître. 
 -- A quelle ligne se situe le message ? Comment trouver cette ligne instantanément ?
 -- Comprenez-vous ce message ? Comment pouvez-vous faire pour le comprendre si l’anglais n’est pas votre fort ?
 
-
+USE exo2;
+INSERT INTO nom_table (col1, col2) VALUES (1, "Mon texte 1"); -- OK
+INSERT INTO nom_table (col1, col2) VALUES (1, "Mon texte 2"); -- KO : Duplicata du champ '1' pour la clef 'col1'
 
 -- 5. A l’aide d’une instruction de simple « SELECT », 
 -- afficher les données contenues dans la table du point 1.2.
+
+-- question : auto_incrément
+INSERT INTO nom_table2 (text) VALUES ("Ma valeur texte");
+INSERT INTO nom_table2 VALUES (NULL, "Ma valeur texte");
+
+SELECT * FROM nom_table2;
 
 
